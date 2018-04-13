27245890a512a08f0b54e335a121763aa10b6865
Luiz Ricardo Vicente Rita <luiz.rita@amcom.com.br>
2018-04-13 16:44:46 -0300

    Jogada especial de roque

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -24,28 +24,64 @@ namespace xadrez {
         }
 
         public Peca executaMovimento(Posicao origem, Posicao destino) {
-            Peca p = tab.retirarPeca(origem);
-            p.incrementarQteMovimentos();
+            Peca P = tab.retirarPeca(origem);
+            P.incrementarQteMovimentos();
             Peca pecaCapturada = tab.retirarPeca(destino);
-            tab.colocarPeca(p, destino);
+            tab.colocarPeca(P, destino);
 
             if (pecaCapturada != null) {
                 capturadas.Add(pecaCapturada);
+            }
+
+            // #jogadaespecial roque pequeno
+            if (P is Rei && destino.coluna == origem.coluna + 2) {
+                Posicao origemT = new Posicao(origem.linha, origem.coluna + 3);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna + 1);
+                Peca T = tab.retirarPeca(origemT);
+                T.incrementarQteMovimentos();
+                tab.colocarPeca(T, destinoT);
+            }
+
+            // #jogadaespecial roque grande
+            if (P is Rei && destino.coluna == origem.coluna - 2) {
+                Posicao origemT = new Posicao(origem.linha, origem.coluna - 4);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna - 1);
+                Peca T = tab.retirarPeca(origemT);
+                T.incrementarQteMovimentos();
+                tab.colocarPeca(T, destinoT);
             }
 
             return pecaCapturada;
         }
 
         public void desfazMovimento(Posicao origem, Posicao destino, Peca pecaCapturada) {
-            Peca p = tab.retirarPeca(destino);
-            p.decrementarQteMovimentos();
+            Peca P = tab.retirarPeca(destino);
+            P.decrementarQteMovimentos();
 
             if (pecaCapturada != null) {
                 tab.colocarPeca(pecaCapturada, destino);
                 capturadas.Remove(pecaCapturada);
             }
 
-            tab.colocarPeca(p, origem);
+            // #jogadaespecial roque pequeno
+            if (P is Rei && destino.coluna == origem.coluna + 2) {
+                Posicao origemT = new Posicao(origem.linha, origem.coluna + 3);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna + 1);
+                Peca T = tab.retirarPeca(destinoT);
+                T.decrementarQteMovimentos();
+                tab.colocarPeca(T, origemT);
+            }
+
+            // #jogadaespecial roque grande
+            if (P is Rei && destino.coluna == origem.coluna - 2) {
+                Posicao origemT = new Posicao(origem.linha, origem.coluna - 4);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna - 1);
+                Peca T = tab.retirarPeca(destinoT);
+                T.decrementarQteMovimentos();
+                tab.colocarPeca(T, origemT);
+            }
+
+            tab.colocarPeca(P, origem);
         }
 
         public void realizaJogada(Posicao origem, Posicao destino) {
