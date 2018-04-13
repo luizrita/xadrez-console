8f5b09cc8544728f74ed08320f55bdb8a3ddba30
Luiz Ricardo Vicente Rita <luiz.rita@amcom.com.br>
2018-04-13 08:21:11 -0300

    Começando a implementar o tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -5,9 +5,9 @@ namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
 
-            Posicao P = new Posicao(3, 4);
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine("Posicao: " + P);
+            
             Console.ReadLine();
 
         }
---
+++ xadrez-console/tabuleiro/Cor.cs
@@ -0,0 +1,13 @@
+﻿namespace tabuleiro {
+    enum Cor {
+
+        Amarela,
+        Azul,
+        Branca,
+        Laranja,
+        Preta,
+        Vermelha,
+        Verde
+
+    }
+}
